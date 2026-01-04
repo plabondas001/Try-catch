e188e4efc8cd0e682efa9fd7ff08e5ef02cf49f9
plabondas001 <plabondas001@gmail.com>
2026-01-05 02:10:25 +0600

    finally block

--- try.js
+++ try.js
@@ -1,17 +1,16 @@
 try {
-    console.log('Hello');
-    console.log(world);
-    console.log(`My name is Plabon`);
-    
-    
-    
+  console.log("Hello");
+  console.log(world);
 } catch (error) {
-    // error
-    console.error(error);
-    // kon type er error ei ta dekhar jonno
-    console.log(error.name);
-    // error message
-    console.log(error.message);
-    
-    
+  // error
+  console.error(error);
+  // kon type er error ei ta dekhar jonno
+  console.log(error.name);
+  // error message
+  console.log(error.message);
+} 
+
+// finally block
+finally {
+  console.log(`My name is Plabon`);
 }
